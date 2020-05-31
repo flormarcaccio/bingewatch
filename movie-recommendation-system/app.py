7c12463d73ddac8c9f222888d999f13858f7a870
Sanjana Gupta <sanjana.1125@gmail.com>
2020-05-31 14:07:55 -0700

    updated dashboard

--- movie-recommendation-system/app.py
+++ movie-recommendation-system/app.py
@@ -2,14 +2,13 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
-import helper_functions
+#import helper_functions
 import imdb
 from dash.dependencies import Input, Output
 
-df = pd.read_csv('./data/imdb_df.csv')
+df_imdb = imdb.load_data()
+genres = imdb.load_genres()
 
-genres = list(helper_functions.get_unique_genres(df))
-year = list((df['startYear'].unique()).astype(int))
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -26,20 +25,32 @@ app.layout = html.Div([
                 style={'marginBottom': 30, 
                        'marginTop': 25, 
                        'fontSize':18}),
+
+            html.Div([
+                html.Label('Filter by:'),
+                dcc.Checklist(
+                        id = 'filter-checklist',
+                        options=[
+                            {'label': 'Genre', 'value': 'Genre'},
+                            {'label': 'Year', 'value': 'Year'}
+                        ],
+                        value=['Genre', 'Year']
+                    )
+
+                ],
+                style={'margin-bottom': '50px',
+                        'margin-left':'20px'}),
+
             html.Div(id = 'slider-wrapper', children = 
              [
                 html.Label('Year:'),
                  dcc.Slider
                 (
                     id='year-slider',
-                    #min=df['startYear'].min(),
-                    #max=df['startYear'].max(),
-                    min = int(df['startYear'].min()),
-                    max = int(df['startYear'].max()),
-                    value = int(df['startYear'].max()),
-                    #marks={str(startYear): str(startYear) for startYear in year},
+                    min = int(df_imdb['startYear'].min()),
+                    max = int(df_imdb['startYear'].max()),
+                    value = int(df_imdb['startYear'].max()),
                     included=False,
-                    #step=1000,
                     updatemode = 'drag',
                     tooltip = {'always_visible': True}
                 ),
@@ -48,47 +59,34 @@ app.layout = html.Div([
                    'margin-left': '20px',
                    'margin-right': '20px',
                    'text-orientation': 'mixed'}),
-      html.Div([dcc.Markdown('Select the genre you would like to see:'),
-                ]),
         
         html.Div([  
             html.Div([
-                #html.Label('Select the genre you would like to see:'),
+                html.Label('Select the genre you would like to see:'),
                     dcc.Dropdown(
                      id = 'genre-dropdown',
                      options = [{'label': i, 'value': i} for i in genres],
-                     #multi = True,
-                     #value = 'Action',
                      placeholder = 'Select Genre',
-                     #style={'height': '40px', 'width': '300px', 'align-items': 'center', 'justify-content':'center'}
-                 )
-                 ], 
-                style={'height': '60px', 
-                   'width': '500px',
-                   'marginTop': 15, 
-                   'fontSize':15, 
-                   'text-align': 'center',
-                   'display' : 'inline-block'
-                   }),
+                 )],
+                    style={'width': '48%',
+                            'margin-bottom': '50px',
+                            'margin-left':'20px',
+                            'display': 'inline-block'}
+            ),
                
             html.Div([
-            html.Label('Select the type of content you would like to see:'),
-                dcc.RadioItems(
-                    id='title-type',
-                    #options=[{'label': i, 'value': i} for i in df['titleType'].unique()],
-                    options = [{'label': 'Movie', 'value' : 'movie'},
-                                {'label' :'TV Series', 'value': 'tvSeries'}],
-                    value='movie',
-                    labelStyle={'display': 'inline-block', 'padding': '10px'}
-            )],
-        style={ 'height' : '60px',
-               'width' : '800px',
-                'display' : 'inline-block',
-               'text-align': 'center',
-               'marginTop': 15, 
-               'fontSize':15}
-        ),
+                html.Label('Select the type of content you would like to see:'),
+                    dcc.RadioItems(
+                        id='title-type',
+                        options = [{'label': 'Movie', 'value' : 'movie'},
+                                    {'label' :'TV Series', 'value': 'tvSeries'}],
+                        value='movie',
+                        labelStyle={'display': 'inline-block', 'padding': '10px'}
+                )],
+                    style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
+            ),
         ]),
+
         html.Div([
             dcc.Graph(style={'height': '400px', 
                              'width': '1300px', 
@@ -102,25 +100,30 @@ app.layout = html.Div([
     
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    [Input('year-slider', 'value'),
+    [Input('filter-checklist', 'value'),
+    Input('year-slider', 'value'),
     Input('title-type', 'value'),
     Input('genre-dropdown', 'value')
    ])
 
   
-def update_figure(selected_year, selected_type, selected_genre):
-    if selected_genre and selected_year:
-        movie_df = imdb.genre_year_df(selected_genre, selected_year, selected_type)
-        final_df = movie_df
-        #print(movie_df)
-    elif selected_genre:
-        genre_df = imdb.genre_df(selected_genre, selected_type)
-        final_df = genre_df
-        #print(genre_df)    
-    elif selected_year:
-        year_df = imdb.year_df(selected_year, selected_type)
-        final_df = year_df
-       # print(year_df)
+def update_figure(selected_filters, selected_year, selected_type, selected_genre):
+
+    genre_filtered = imdb.filter_selected(selected_filters, 'Genre')
+    year_filtered = imdb.filter_selected(selected_filters, 'Year')
+    
+    final_df = imdb.filter_type(df_imdb, selected_type)
+
+
+    if genre_filtered and selected_genre:
+        final_df = imdb.filter_genre(final_df, selected_genre)
+    if year_filtered and selected_year:
+        final_df = imdb.filter_year(final_df, selected_year)
+
+    final_df = imdb.filter_top10(final_df)
+
+
+
     return {
         'data': [{'x': final_df['primaryTitle'], 'y' : final_df['weightedAverage'], 'type' : 'bar'}],
         'layout': dict(
--- movie-recommendation-system/imdb.py
+++ movie-recommendation-system/imdb.py
@@ -6,48 +6,30 @@ Created on Mon May 18 17:52:12 2020
 """
 
 import pandas as pd
+import pickle
 
-imdb_dt = pd.read_csv('data/imdb_df.csv')
-imdb_dt['startYear'] = imdb_dt['startYear'].astype(int)
 
-def matchGenre(genre:str):
-  imdb_dt1 = imdb_dt[imdb_dt['genres']==genre]
-  imdb_dt1 = imdb_dt1.sort_values(by='weightedAverage', ascending=False)
-  movielist = imdb_dt1.primaryTitle.to_numpy()
-  if len(movielist)>10:
-    movielist = movielist[0:10]
-  return movielist
+def load_data():
+    imdb_df = pd.read_csv('data/processed/imdb_df.csv')
+    return imdb_df
 
-def matchYear(yr:int):
-  imdb_dt2 = imdb_dt[imdb_dt['startYear']==yr]
-  imdb_dt2 = imdb_dt2.sort_values(by='weightedAverage', ascending=False)
-  movielist2 = imdb_dt2.primaryTitle.to_numpy()
-  if len(movielist2)>10:
-    movielist2 = movielist2[0:10]
-  return movielist2
+def load_genres():
+    with open('data/processed/set_genres.pkl', 'rb') as f:
+        genres_set = pickle.load(f)
+    return list(genres_set)
 
-def genre_df(genre:str, titletype:str):
-    imdb_dt3 = imdb_dt[imdb_dt['genres'].str.contains(genre) 
-                       & imdb_dt['titleType'].str.contains(titletype)]
-    filtered_df = imdb_dt3.nlargest(10, 'weightedAverage')
-    return filtered_df
+def filter_type(data, titletype:str):
+    return data[data['titleType'].str.contains(titletype)]
+    
 
-def year_df(yr:int, titletype:str):
-    imdb_dt4 = imdb_dt[imdb_dt['startYear'].astype(int) == yr]
-    imdb_dt4 = imdb_dt4[imdb_dt4['titleType'].str.contains(titletype)]
-    filtered_df = imdb_dt4.nlargest(10, 'weightedAverage')
-    return filtered_df
+def filter_genre(data, genre:str):
+    return data[data['genres'].str.contains(genre)]
 
-def genre_year_df(genre:str, yr:int, titletype:str):
-    imdb_dt5 = imdb_dt[(imdb_dt['genres'].str.contains(genre)) & 
-                       (imdb_dt['titleType'].str.contains(titletype)) & 
-                       (imdb_dt['startYear'].astype(int) == yr)]    
-    #print(imdb_dt5)
-    filtered_df = imdb_dt5.nlargest(10, 'weightedAverage')
-    #print(filtered_df)
-    return filtered_df
+def filter_year(data, yr:int):
+    return data[data['startYear'].astype(int) == yr]
 
-#genre_df('Action', 'tvSeries')
-#print(year_df(2010, 'tvSeries'))
-#print(genre_year_df('Action', 2010, 'tvSeries'))
-#print(imdb_dt['startYear'].type)
+def filter_top10(data):
+    return data.nlargest(10, 'weightedAverage')
+
+def filter_selected(list_values, value):
+    return value in list_values
