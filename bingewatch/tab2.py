4b2bc23396ab2f69b8f8074a1afab3d9e2be55ee
Sanjana Gupta <sanjana.1125@gmail.com>
2020-06-02 14:28:19 -0700

    fixed tab2 issues

--- bingewatch/tab2.py
+++ bingewatch/tab2.py
@@ -1,7 +1,6 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import pandas as pd
 import imdb
 from dash.dependencies import Input, Output
 
@@ -14,75 +13,66 @@ genres = imdb.load_genres(GENRES_PATH)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 tab2_layout = html.Div([
-            html.Div([
-                html.Label('Filter by:'),
-                dcc.Checklist(
+                html.Div([
+                    html.Label('Filter by:'),
+                    dcc.Checklist(
                         id = 'filter-checklist',
                         options=[
                             {'label': 'Genre', 'value': 'Genre'},
                             {'label': 'Year', 'value': 'Year'}
                         ],
                         value=['Genre', 'Year']
-                    )
-
-                ],
-                style={'margin-bottom': '50px',
+                    )],
+                    style={'margin-bottom': '50px',
                         'margin-left':'20px'}),
 
-            html.Div(id = 'slider-wrapper', children = 
-             [
-                html.Label('Year:'),
-                 dcc.Slider
-                (
-                    id='year-slider',
-                    min = int(df_imdb['startYear'].min()),
-                    max = int(df_imdb['startYear'].max()),
-                    value = int(df_imdb['startYear'].max()),
-                    included=False,
-                    updatemode = 'drag',
-                    tooltip = {'always_visible': True}
-                ),
-            ],
-            style={'margin-bottom': '50px',
-                   'margin-left': '20px',
-                   'margin-right': '20px',
-                   'text-orientation': 'mixed'}),
-        
-        html.Div([  
-            html.Div([
-                html.Label('Select the genre you would like to see:'),
-                    dcc.Dropdown(
-                     id = 'genre-dropdown',
-                     options = [{'label': i, 'value': i} for i in genres],
-                     placeholder = 'Select Genre',
-                 )],
-                    style={'width': '48%',
+                html.Div(id = 'slider-wrapper', children = [
+                    html.Label('Year:'),
+                    dcc.Slider(
+                        id='year-slider',
+                        min = int(df_imdb['startYear'].min()),
+                        max = int(df_imdb['startYear'].max()),
+                        value = int(df_imdb['startYear'].max()),
+                        included=False,
+                        updatemode = 'drag',
+                        tooltip = {'always_visible': True}
+                    ),],
+                    style={'margin-bottom': '50px',
+                       'margin-left': '20px',
+                       'margin-right': '20px',
+                       'text-orientation': 'mixed'}),
+                html.Div([  
+                    html.Div([
+                        html.Label('Select the genre you would like to see:'),
+                        dcc.Dropdown(
+                             id = 'genre-dropdown',
+                             options = [{'label': i, 'value': i} for i in genres],
+                             placeholder = 'Select Genre',
+                         )],
+                        style={'width': '48%',
                             'margin-bottom': '50px',
                             'margin-left':'20px',
                             'display': 'inline-block'}
-            ),
-               
-            html.Div([
-                html.Label('Select the type of content you would like to see:'),
+                        ),
+                html.Div([
+                    html.Label('Select the type of content you would like to see:'),
                     dcc.RadioItems(
                         id='title-type',
                         options = [{'label': 'Movie', 'value' : 'movie'},
                                     {'label' :'TV Series', 'value': 'tvSeries'}],
                         value='movie',
                         labelStyle={'display': 'inline-block', 'padding': '10px'}
-                )],
+                    )],
                     style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
-            ),
-        ]),
+                ),
+                ]),
 
-        html.Div([
-            dcc.Graph(style={'height': '400px', 
+            html.Div([
+                dcc.Graph(style={'height': '400px', 
                              'width': '1300px', 
                              'margin-left':'auto', 
                              'margin-right':'auto'},
                       id='graph-with-slider')
-                ])
-])
+            ])
+    ])
