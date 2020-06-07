530957c9db6fd7dfbe6f8e7e09aa708d2efb4a33
Florencia Marcaccio <flormarcaccio@gmail.com>
2020-06-06 20:02:43 -0700

    adding tests, fixing paths and documentation

--- bingewatch/tests/tests.py
+++ bingewatch/tests/tests.py
@@ -1,122 +1,219 @@
-import pandas as pd
-import unittest
-import pickle
+"""
+This module tests most of the functiones used throughout the package.
+It is not possible to test the function process_netflix() and process_imdb()
+found in data_manager.py, as these functions ingest and process all of the
+data, which would take too much time. However, we do test all the functions
+used inside process_netflix() and process_imdb().
+"""
 import os
 import sys
-sys.path.append('../data')
+import unittest
+import pickle
+import pandas as pd
 import bingewatch.data.helper_functions as hf
 import bingewatch.imdb as imdb
+sys.path.append('../data')
 
 
-class test_df(unittest.TestCase):
-	"""
-	Check that the unique genres in the test_df
-	have been correctly identified.
-	"""
-	test_df = pd.DataFrame({'movies' : ["A", "B", "C", "D"], 
-			'genres' : ["Action", "Comedy", "Comedy", "Romance"]})
+CSV_EXT = '.csv'
+TAB = '\t'
+NAS = ['\\N']
 
-	def test_unique_genres(self):
-		self.assertEqual(
-			hf.get_unique_genres(self.test_df), 
-			set(["Action", "Comedy", "Romance"]))
+PACKAGE_DIR = 'bingewatch'
+DATA_DIR = 'data'
+TEST_DATA_DIR = 'test_files'
+PROCESSED_DIR = 'processed'
 
-	def test_save_file(self):
-		"""
-		Check that the save_file function correctly outputs a csv file,
-		then remove the created file.
-		"""
-		hf.save_file(self.test_df, "./", "test_file", ".csv")
-		self.assertTrue(os.path.exists('./test_file.csv'))
-		os.system("rm test_file.csv")
+IMDB_TEST = 'imdb_df_test.csv'
+IMDB_TITLES_TEST = 'imdb_titles_test.tsv'
+IMDB_RATINGS_TEST = 'imdb_ratings_test.tsv'
+IMDB_MERGED_COLS = ['tconst', 'titleType', 'primaryTitle', 'startYear',
+                    'genres', 'averageRating', 'numVotes', 'weightedAverage']
+GENRES_TEST = 'set_genres_test.pkl'
+NF_RATINGS_TEST = 'netflix_test.txt'
+NF_RATINGS_COLS = ['movie_id', 'user_id', 'rating', 'rating_date']
+MOVIE_TITLES_TEST = 'movie_titles_test.csv'
+NF_MOVIE_TITLES_COLS = ['Sno', 'Year', 'Final_title', 'Display']
 
-	def test_download_gz_file(self):
-		imdb_url = 'https://datasets.imdbws.com/title.ratings.tsv.gz'
-		generated_df = hf.download_gz_file(imdb_url)
-		self.assertGreater(len(generated_df), 0)
+TEST_FILE_OUT = 'test_file'
 
-	def test_parse_data(self):
-		lst_data = hf.parse_data('bingewatch/data/test_files/netflix_test.txt')
-		self.assertEqual(len(lst_data), 10)
-
-	def test_get_recommended_movies(self):
-		lst_data = hf.parse_data('bingewatch/data/test_files/netflix_test.txt')
-		df_nf_cols = ['movie_id', 'user_id', 'rating', 'rating_date']
-		df_netflix = pd.DataFrame(lst_data, columns=df_nf_cols)
-		self.assertEqual(len(hf.get_recommended_movies(df_netflix)), 3)
-
-	def test_format_movie_titles(self):
-		pass
-
-	def test_clean_imdb_data(self):
-		pass
-
-	def test_download_netflix_data(self):
-		pass
+IMDB_URL = 'https://datasets.imdbws.com/title.ratings.tsv.gz'
 
 
+class TestHelperFunctions(unittest.TestCase):
+    """
+    Test all the functions in helper_functions.py.
+    """
+    test_df = pd.DataFrame({'movies' : ["A", "B", "C", "D"],
+                            'genres' : ["Action,Romance", "Comedy",
+                                        "Comedy", "Romance"]})
+
+    def test_unique_genres(self):
+        """
+        Check that the unique genres in the test_df have been correctly
+        identified with the get_unique_genres(df) function.
+        """
+        self.assertEqual(
+            hf.get_unique_genres(self.test_df),
+            set(["Action", "Comedy", "Romance"]))
+
+    def test_save_file(self):
+        """
+        Check that the save_file(file) function correctly outputs a csv file,
+        then remove the created file.
+        """
+        hf.save_file(self.test_df, "./", "test_file", ".csv")
+        self.assertTrue(os.path.exists('./test_file.csv'))
+        os.system("rm test_file.csv")
+
+    def test_download_gz_file(self):
+        """
+        Checks that the function download_gz_file(url) generates a
+        non-empty dataframe.
+        """
+        generated_df = hf.download_gz_file(IMDB_URL)
+        self.assertGreater(len(generated_df), 0)
+
+    def test_parse_data(self):
+        """
+        Checks that the function parse_data(file) parses the data from the file
+        and outputs a list with the 10 ratings found in the test file.
+        """
+        file_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, NF_RATINGS_TEST)
+        lst_data = hf.parse_data(file_path)
+        self.assertEqual(len(lst_data), 10)
+
+    def test_get_recommended_movies(self):
+        """
+        Checks that the function get_recommended_movies(df) outputs a dictionary with
+        recommendations for the 3 movies in the test file.
+        """
+        file_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, NF_RATINGS_TEST)
+        lst_data = hf.parse_data(file_path)
+        df_netflix = pd.DataFrame(lst_data, columns=NF_RATINGS_COLS)
+        self.assertEqual(len(hf.get_recommended_movies(df_netflix)), 3)
+
+    def test_format_movie_titles(self):
+        """
+        Checks that the function format_movie_titles(file) processes the
+        test file and adds the necessary columns. This function checks
+        that the columns match with the desired output.
+        """
+        file_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, MOVIE_TITLES_TEST)
+        movie_titles = hf.format_movie_titles(file_path)
+        print(movie_titles)
+        self.assertEqual(list(movie_titles.columns), NF_MOVIE_TITLES_COLS)
+
+    def test_clean_imdb_data(self):
+        """
+        Checks that the function clean_imdb_data(df1, df2) processes and merges the
+        test dataframes and adds calculates the necessary columns. This function checks
+        that the columns match with the desired output.
+        """
+        titles_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, IMDB_TITLES_TEST)
+        ratings_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, IMDB_RATINGS_TEST)
+        titles_df = pd.read_csv(titles_path, sep=TAB, na_values=NAS)
+        ratings_df = pd.read_csv(ratings_path, sep=TAB, na_values=NAS)
+        merged_df = hf.clean_imdb_data(titles_df, ratings_df)
+        self.assertEqual(list(merged_df.columns), IMDB_MERGED_COLS)
+
+    def test_download_netflix_data(self):
+        pass
 
 
-class test_imdb(unittest.TestCase):
 
-	test_df = pd.DataFrame({
-		'movies' : ["A", "B", "C", "D", "E", "F",
-            "G", "H", "I", "J", "K"], 
-		'titleType': ["Movie", "Movie", "Movie", "tvSeries",
-		    "tvSeries", "tvSeries", "tvSeries", "Movie",
-		    "tvSeries", "Movie", "Movie"],
+
+class TestImdb(unittest.TestCase):
+    """
+    Test all the function in imdb.py.
+    """
+    test_df = pd.DataFrame({
+        'movies' : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"],
+        'titleType': ["Movie", "Movie", "Movie", "tvSeries", "tvSeries",
+                      "tvSeries", "tvSeries", "Movie", "tvSeries", "Movie",
+                      "Movie"],
         'genres' : ["Action", "Comedy", "Comedy", "Romance",
-            "Comedy, Romance", "Comedy, Action", "Romance",
-            "Action, Comedy", "Action", "Action", "Romance"],
-        'startYear':[2020, 2019, 2018, 2020, 2016, 2015, 2012,
-            2017, 2016, 2019, 2018],
+                    "Comedy,Romance", "Comedy,Action", "Romance",
+                    "Action,Comedy", "Action", "Action", "Romance"],
+        'startYear':[2020, 2019, 2018, 2020, 2016, 2015, 2012, 2017,
+                     2016, 2019, 2018],
         'weightedAverage':[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]})
 
-	def test_load_data(self):
-		original_file = pd.read_csv('bingewatch/data/processed/imdb_df.csv')
-		self.assertEqual(len(imdb.load_data('bingewatch/data/processed/imdb_df.csv')),
-			len(original_file))
+    def test_load_data(self):
+        """
+        Checks that the function load_data(file) properly loads the desired file.
+        """
+        file_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, IMDB_TEST)
+        original_file = pd.read_csv(file_path)
+        self.assertEqual(len(imdb.load_data(file_path)), len(original_file))
 
-	def test_load_genres(self):
-		with open('bingewatch/data/processed/set_genres.pkl', 'rb') as f:
-			original_file = pickle.load(f)
-		self.assertEqual(imdb.load_genres('bingewatch/data/processed/set_genres.pkl'),
-			list(original_file))
+    def test_load_genres(self):
+        """
+        Checks that the function load_genres(file) properly loads the list of genres.
+        """
+        file_path = os.path.join(PACKAGE_DIR, DATA_DIR, TEST_DATA_DIR, GENRES_TEST)
+        with open(file_path, 'rb') as file:
+            original_file = pickle.load(file)
+        self.assertEqual(imdb.load_genres(file_path), list(original_file))
 
-	def test_filter_type(self):
-		self.assertEqual(len(imdb.filter_type(self.test_df, 'Movie')), 6)
+    def test_filter_type(self):
+        """
+        Checks that the function filter_type(df, str) filters test_df
+        correctly by 'titleType'.
+        """
+        self.assertEqual(len(imdb.filter_type(self.test_df, 'Movie')), 6)
 
-	def test_filter_genre(self):
-		self.assertEqual(len(imdb.filter_genre(self.test_df, 'Action')), 5)
+    def test_filter_genre(self):
+        """
+        Checks that the function filter_genre(df, str) filters test_df
+        correctly by 'genres'.
+        """
+        self.assertEqual(len(imdb.filter_genre(self.test_df, 'Action')), 5)
 
-	def test_filter_year(self):
-		self.assertEqual(len(imdb.filter_year(self.test_df, 2018)), 2)
+    def test_filter_year(self):
+        """
+        Checks that the function filter_year(df, int) filters test_df
+        correctly by 'startYear'.
+        """
+        self.assertEqual(len(imdb.filter_year(self.test_df, 2018)), 2)
 
-	def test_filter_top10(self):
-		self.assertGreater(imdb.filter_top10(self.test_df)['weightedAverage'].min(), 1)
+    def test_filter_top10(self):
+        """
+        Checks that the function filter_top10(df) gets the 10 rows with the
+        highest 'weightedAverage' from test_df. As test_df only has 11 rows
+        and the minimum 'weightedAverage' is 1, it checks that the minimum
+        'weightedAverage' from the result is higher than 1.
+        """
+        self.assertGreater(imdb.filter_top10(self.test_df)['weightedAverage'].min(), 1)
 
-	def test_filter_genre(self):
-		self.assertTrue(imdb.filter_selected(["tvSeries", "Movie"], "Movie"))
+    def test_filter_selected(self):
+        """
+        Checks that the function filter_selected(list, string) returns true when
+        the string exists in the list.
+        """
+        self.assertTrue(imdb.filter_selected(["tvSeries", "Movie"], "Movie"))
 
 
 
 
-class test_netflix(unittest.TestCase):
+class TestNetflix(unittest.TestCase):
+    """
+    Test all the function in netflix.py.
+    """
+    def test_reading_movie_title_csv(self):
+        pass
 
-	def test_reading_movie_title_csv(self):
-		pass
+    def test_get_options(self):
+        pass
 
-	def test_get_options(self):
-		pass
+    def test_userchoice_based_movie_recommendation(self):
+        pass
 
-	def test_userchoice_based_movie_recommendation(self):
-		pass
-
-	def test_generate_table(self):
-		pass
+    def test_generate_table(self):
+        pass
 
 
 
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
